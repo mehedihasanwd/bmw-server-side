af9518108c83a8a9fa6fb3d52450a0d027694b93
mehedihasanwd <hellomehediwd@gmail.com>
2021-11-09 23:24:03 +0600

    Hello World

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB - URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qbhxv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+// MongoDB - Client
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("BMW");
+    const bmwCollection = database.collection("Cars");
+    const bmwDemo = database.collection("CarsDemo");
+
+    // Test
+    const doc = {
+      name: "Mehedi",
+      email: "hellomehediwd@gmail.com",
+    };
+    const result = await bmwDemo.insertOne(doc);
+    console.log(result);
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Default Route
+app.get("/", (req, res) => {
+  res.send("Running the BMW Server");
+});
+
+// Listening to the app
+app.listen(port, () => {
+  console.log("Listening to the Port: ", port);
+});
