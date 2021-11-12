8336c7aa0395f416f16ea89ee25b66f39a528eec
mehedihasanwd <hellomehediwd@gmail.com>
2021-11-13 01:04:12 +0600

    Hello World

--- index.js
+++ index.js
@@ -136,6 +136,7 @@ async function run() {
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await userCollection.insertOne(user);
+      console.log(result);
       res.json(result);
     });
     // Upsert User - API
